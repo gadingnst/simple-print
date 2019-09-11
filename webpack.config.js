07212aec3230c1d32dc7ac693af6213c3025628c
Sutan Nasution <sutan.gnst@gmail.com>
2019-09-12 02:55:46 +0700

    resolve webpack entry

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: ['./lib/index.js'],
   mode: 'production',
   output: {
     path: path.join(__dirname, 'lib', 'bundle'),
